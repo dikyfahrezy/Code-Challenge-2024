d602d2fa37b7fc829f0387274622ea1b5c901859
dikyfahrezy <dikyfahrezy@gmail.com>
2024-01-27 20:28:37 +0700

    Challenge 7/366

---
+++ CodeWars/7-number to string.js
@@ -0,0 +1,14 @@
+function numberToString(num) {
+  return `${num}`; //return ("" + num);
+}
+
+// Method toString pada JavaScript
+// method ini tidak dapat mengembalikan nilai null dan undefined
+// const numberToString = (num) => num.toString();
+
+//dapat digunakan ketika nilai atau valuenya null atau undefined
+// function numberToString(num) {
+//   return String(num);
+// }
+
+console.log(numberToString(67));
