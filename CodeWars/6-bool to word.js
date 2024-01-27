335fcd20d6454164c90eb80c939de1208dddfc39
dikyfahrezy <dikyfahrezy@gmail.com>
2024-01-27 19:50:29 +0700

    day 3 Challenge 6/366

---
+++ CodeWars/6-bool to word.js
@@ -0,0 +1,14 @@
+// function boolToWord(bool) {
+//   return bool ? "Yes" : "No";
+// }
+
+// function boolToWord(bool) {
+//   if (bool) {
+//     return "Yes";
+//   } else return "No";
+// }
+
+const boolToWord = (bool) => (bool ? "Yes" : "No");
+
+console.log(boolToWord(true));
+console.log(boolToWord(false));
